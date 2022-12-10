c0d5e7791828c6aa28c871e612f577f4931300df
aonetoy <90669873+aonetoy@users.noreply.github.com>
2022-12-10 21:27:00 +0900

    맵 연동 완료
    맵 연동 완료
    각 모드 별로 스폰 위치, 시간 제한 등 수정
    게임 대기실 디자인 수정 및 해상도 오류 수정
    Web Page로 이동 추가
    
    Co-Authored-By: mvashe <84505267+mvashe@users.noreply.github.com>

---
+++ Unity/Assets/Karting/Scripts/HomePage.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HomePage : MonoBehaviour
+{
+    public void Connect(){
+        Application.OpenURL("www.saladball.cf");
+    }
+}
